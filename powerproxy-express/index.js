bde5da284b4499e2d932b54fe19249580d60ae0b
Thomas Hammerl <thomas.hammerl@semanticlabs.at>
2022-06-13 17:49:35 +0200

    Add credentialsRequired option to powerproxy-express middleware
     - To support applications with optional authentication (e.g. our placeholder-site).

--- powerproxy-express/index.js
+++ powerproxy-express/index.js
@@ -1,13 +1,14 @@
-const { expressjwt: jwt } = require("express-jwt");
+const {expressjwt: jwt} = require("express-jwt");
 const cookie = require("cookie");
 
-function powerproxy({secret}) {
+function powerproxy({secret, credentialsRequired = false}) {
     return function (req, res, next) {
         const cookies = req.headers.cookie ? cookie.parse(req.headers.cookie) : {};
         jwt({
             secret,
             algorithms: ["HS256"],
-            getToken: () => cookies["__pp__token__"]
+            getToken: () => cookies["__pp__token__"],
+            credentialsRequired,
         })(req, res, (error) => {
             if (error?.name === "UnauthorizedError") {
                 res.redirect(303, "/.pp/login");
